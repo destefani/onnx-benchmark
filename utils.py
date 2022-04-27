c3f661fc55d3c4fbe45c0d474e07af9e1b4d3020
Marcelo Destefani <marcelo@hastapronto.cl>
2022-04-27 10:38:37 +0200

    utils removed

--- utils.py
+++
@@ -1,9 +0,0 @@
-import re
-import time
-
-def execution_time(process):
-    """ Calculates the execution time of a function """
-    start_time = time.time()
-    (lambda: process)()
-    end_time = time.time()
-    return (end_time - start_time)
